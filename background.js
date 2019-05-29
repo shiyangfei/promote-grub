e7b49e3446048b754a258c31338fbc1c4e3276e1
Shiyang Fei <shiyang@compass.com>
2019-05-29 14:48:05 -0400

    bug fixed

--- background.js
+++ background.js
@@ -2,10 +2,18 @@ let currentTab;
 const version = "1.0";
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  if (!window.grubDict) {
+    window.grubDict = {}
+  }
   if (changeInfo.status == 'complete') {
     const { url } = tab
     console.log(`page url: ${url}`)
     if (url.indexOf('https://www.grubhub.com/restaurant/') === 0) {
+      const managed_delivery = window.grubDict[url]
+      if (managed_delivery) {
+        runShowPopupScript(managed_delivery)
+        return
+      }
       chrome.tabs.query({
         currentWindow: true,
         active: true
@@ -28,10 +36,13 @@ function onAttach(tabId) {
 }
 
 function allEventHandler(debuggeeId, message, params) {
+  console.log('event triggered')
   if (currentTab.id != debuggeeId.tabId) {
+    console.log('tabId does not match')
     return;
   }
   if (message == "Network.responseReceived") {
+    console.log(message)
     const url = params.response.url
     if (url.indexOf('https://api-gtm.grubhub.com/restaurants/') < 0) {
       return
@@ -45,20 +56,28 @@ function allEventHandler(debuggeeId, message, params) {
         if (!res) {
           return
         }
+        console.log('network body received')
         const { body } = res
         if (body && body.indexOf('managed_delivery') > -1) {
+          console.log('managed_delivery received')
           let managed_delivery = JSON.parse(body).restaurant.managed_delivery
           managed_delivery = managed_delivery === true ? 'yes' : 'no'
-          console.log(managed_delivery)
-          chrome.tabs.executeScript({
-            code: `window.managed_delivery = '${managed_delivery}'`
-          }, function () {
-            chrome.tabs.executeScript({
-              file: "showPopup.js"
-            });
-          });
+          console.log(`managed_delivery: ${managed_delivery}`)
+          const url = currentTab.url
+          window.grubDict[url] = managed_delivery
+          runShowPopupScript(managed_delivery)
           chrome.debugger.detach(debuggeeId)
         }
       });
   }
+}
+
+function runShowPopupScript(managed_delivery) {
+  chrome.tabs.executeScript({
+    code: `window.managed_delivery = '${managed_delivery}'`
+  }, function () {
+    chrome.tabs.executeScript({
+      file: "showPopup.js"
+    });
+  });
 }
